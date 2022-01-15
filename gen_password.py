57e0d8d2bbd900500c1e17063e8983a4577f02a4
Albert Visagie <albert@vastech.co.za>
2022-01-15 13:05:14 +0200

    first light

---
+++ gen_password.py
@@ -0,0 +1,45 @@
+import secrets
+import math
+
+consonants = 'cdfghjklmnpqrstvwxyz'
+consonants += consonants.upper()
+vowels = 'aeiouy'
+numbers = '1234567890'
+symbols = '!$%^&*-'
+
+
+def entropy(p):
+    entropy = 0
+    for l in p:
+        entropy += -math.log2(1/l)
+
+    return entropy
+
+
+def gen(nsyllables=4, nsymbols=1, nnumbers=2):
+    def add(lengths, options):
+        lengths.append(len(options))
+        return secrets.choice(options)
+
+    lengths = []
+    pwd = ""
+
+    # "syllables"
+    for x in range(nsyllables):
+        pwd += add(lengths, consonants)
+        pwd += add(lengths, vowels)
+
+    # symbol
+    for x in range(nsymbols):
+        pwd += add(lengths, symbols)
+
+    # some numbers
+    for x in range(nnumbers):
+        pwd += add(lengths, numbers)
+
+    return pwd, entropy(lengths)
+
+
+p, entr = gen()
+print(f"{p}")
+print(f"-> {entr:0.01f} bits")
